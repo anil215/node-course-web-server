71ea33fcc26df14d583a5e54fec9270565957a90
anil <anilbit215@gmail.com>
2016-11-15 14:49:52 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs= require('hbs');
+const fs= require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+// middleware is used to tweak the functionality of express
+// app.set for handlebars and app.use for middlewares
+app.use(express.static(__dirname+'/public'));
+
+app.use((req,res,next) => {
+  var now = new Date().toString();
+  var log=`${now}: ${req.method} ${req.url}`;
+  fs.appendFile('server.log',log + '\n');
+  console.log(log);
+  next();
+});
+
+// app.use((req,res,next) => {
+//   res.render('maintainence.hbs');
+// });
+
+
+
+hbs.registerHelper('currentYear',() => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt',(text) => {
+  return text.toUpperCase();
+});
+
+app.get('/',(req,res) => {
+
+  res.render('home.hbs',{
+    welcomeMessage: 'Welcome to my website',
+    pageTitle: 'Home Page'
+  });
+});
+
+app.get('/about',(req,res) => {
+  res.render('about.hbs',{
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad',(req,res) => {
+
+  res.send({
+    errorMessage :'Unable to fetch the website'
+  });
+});
+app.listen(3000 , ()=> {
+  console.log('SErver is up on port 3000')
+});
